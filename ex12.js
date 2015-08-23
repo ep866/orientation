bf6778b3616e69bf654c59396333f4a526addf60
paune866 <paune866@newschool.edu>
2015-08-23 20:47:30 +0000

    add exercise 12

---
+++ ex12.js
@@ -0,0 +1,19 @@
+var http = require('http'),
+    map = require('through2-map');
+    
+var port = process.argv[2];
+
+var server = http.createServer( function(req, res) {
+    
+    if(req.method != 'POST') {
+        return res.end();
+    }
+    
+    req.pipe( map(function(chunk) {
+        //date manipulation, follwed by repipe
+       return chunk.toString().toUpperCase();
+    })).pipe(res);
+    
+});
+
+server.listen(port);
